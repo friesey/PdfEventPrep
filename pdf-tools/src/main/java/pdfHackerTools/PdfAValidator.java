07b7096e33778cdcbd3cc1503afea2b0fd6c9c79
friesey <y.friese@zbw.eu>
2014-08-28 13:29:36 +0200

    PdfAValidator: Adding an Analysis of the error messages + count how many PDF/A are valid/invalid

--- pdf-tools/src/main/java/pdfHackerTools/PdfAValidator.java
+++ pdf-tools/src/main/java/pdfHackerTools/PdfAValidator.java
@@ -1,11 +1,15 @@
 package pdfHackerTools;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.activation.FileDataSource;
 
@@ -17,28 +21,32 @@ import org.apache.pdfbox.preflight.parser.PreflightParser;
 
 public class PdfAValidator {
 
-	static String t;
+	static String examinedFolder;
 	static PrintWriter outputfile;
 	static PrintWriter ShortSummary;
+
+	// TODO: are these Constants and should be put in CAPITAL LETTERS?
+	static String valid = " is a valid PDF/A-1b file";
+	static String invalid = " is not valid, error(s) :";
 
 	public static void main(String args[]) throws IOException {
 
 		try {
 
-			t = PdfUtilities.chooseFolder();
+			examinedFolder = PdfUtilities.chooseFolder();
 
 			// Generating two Outputfiles in the folder that is examined
 
-			outputfile = new PrintWriter(new FileWriter(t + "//"
+			outputfile = new PrintWriter(new FileWriter(examinedFolder + "//"
 					+ "PdfAValidation.txt"));
 
-			ShortSummary = new PrintWriter(new FileWriter(t + "//"
+			ShortSummary = new PrintWriter(new FileWriter(examinedFolder + "//"
 					+ "PdfAValidationShortSummary.txt"));
 
-			if (t != null) {
+			if (examinedFolder != null) {
 
-				ArrayList<File> files = PdfUtilities.getPaths(new File(t),
-						new ArrayList<File>());
+				ArrayList<File> files = PdfUtilities.getPaths(new File(
+						examinedFolder), new ArrayList<File>());
 
 				for (int i = 0; i < files.size(); i++) {
 					if (!files.get(i).isDirectory() && files.get(i) != null) {
@@ -55,7 +63,7 @@ public class PdfAValidator {
 
 							if (PdfUtilities.testPdfOk(files.get(i)))
 							/*
-							 * Test if the Pdf File is ok to be examined.
+							 * Test if the Pdf File is OK to be examined.
 							 * Otherwise gives error in Console
 							 */
 							{
@@ -103,20 +111,14 @@ public class PdfAValidator {
 															+ files.get(i)
 															+ " is a valid PDF/A-1b file");
 
-											ShortSummary
-													.println("The file "
-															+ files.get(i)
-															+ " is a valid PDF/A-1b file");
+											ShortSummary.println("The file "
+													+ files.get(i) + valid);
 										} else {
-											outputfile
-													.println("The file "
-															+ files.get(i)
-															+ " is not valid, error(s) :");
+											outputfile.println("The file "
+													+ files.get(i) + valid);
 
-											ShortSummary
-													.println("The file "
-															+ files.get(i)
-															+ " is not valid, error(s) :");
+											ShortSummary.println("The file "
+													+ files.get(i) + invalid);
 											for (ValidationError error : result
 													.getErrorsList()) {
 												outputfile.println(error
@@ -137,8 +139,64 @@ public class PdfAValidator {
 					}
 				}
 			}
-			ShortSummary.close();
+
 			outputfile.close();
+
+			// TODO: Analyze Results / Statistics
+
+			/*
+			 * Analyzing the just-generated outputfile with all the findings of
+			 * the PDF/A-Validation-Test
+			 */
+
+			FileInputStream inputStream = new FileInputStream(examinedFolder
+					+ "//" + "PdfAValidation.txt");
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					inputStream));
+
+			String line = null;
+
+			int countValidPdf = 0;
+			int countInvalidPdf = 0;
+
+			ArrayList<String> errorlist = new ArrayList<String>();
+			ArrayList<String> lines = new ArrayList<String>();
+
+			StringBuilder responseData = new StringBuilder();
+			while ((line = reader.readLine()) != null) {
+				responseData.append(line);
+				lines.add(line);
+			}
+
+			int len = lines.size();
+
+			for (int i = 0; i < len; i++) {
+				if (lines.get(i).contains(valid)) {
+					countValidPdf++;
+				} else if (lines.get(i).contains(invalid)) {
+					countInvalidPdf++;
+				} else if (lines.get(i).contains("ErrorMessage")) {
+					errorlist.add(lines.get(i));
+				}
+			}
+
+			System.out.println("valid PDF/A files: " + countValidPdf);
+			ShortSummary.println("valid PDF/A files: " + countValidPdf);
+			System.out.println("invalid PDF files: " + countInvalidPdf);
+			ShortSummary.println("invalid PDF files: " + countInvalidPdf);
+			reader.close();
+			
+			Collections.sort(errorlist);
+			
+			//Errors will be sorted, counted and written in ShortSummary
+			
+			
+			
+			
+
+			ShortSummary.close();
+
 		} catch (FileNotFoundException e) {
 		}
 	}
